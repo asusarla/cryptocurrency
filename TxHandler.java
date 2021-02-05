b851384e8f85665b51902b8e827e86947cfd5bd7
Aruna Susarla <aruna.susarla@gmail.com>
2021-02-05 14:08:59 -0800

    Update TxHandler.java

--- TxHandler.java
+++ TxHandler.java
@@ -1,3 +1,8 @@
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
 public class TxHandler {
 
      private UTXOPool utxoPool;
@@ -12,8 +17,8 @@ public class TxHandler {
 
     /**
      * @return true if:
-     * (1) all outputs claimed by {@code tx} are in the current UTXO pool, 
-     * (2) the signatures on each input of {@code tx} are valid, 
+     * (1) all outputs claimed by {@code tx} are in the current UTXO pool,
+     * (2) the signatures on each input of {@code tx} are valid,
      * (3) no UTXO is claimed multiple times by {@code tx},
      * (4) all of {@code tx}s output values are non-negative, and
      * (5) the sum of {@code tx}s input values is greater than or equal to the sum of its output
@@ -47,7 +52,7 @@ public class TxHandler {
 		for (int i = 0; i < outputs.size(); i++) {
 			Transaction.Output output = outputs.get(i);
             /**(4) all of {@code tx}s output values are non-negative*/
-			if (output.value <= 0) { 
+			if (output.value <= 0) {
 				return false;
 			}
 			outputTotal += output.value;
@@ -56,8 +61,9 @@ public class TxHandler {
           /* * (5) the sum of {@code tx}s input values is greater than or equal to the sum of its output. **/
 		if (outputTotal > inputTotal) {
 			return false;
-		}      
     }
+    return true;
+  }
 
     /**
      * Handles each epoch by receiving an unordered array of proposed transactions, checking each
@@ -66,29 +72,31 @@ public class TxHandler {
      */
     public Transaction[] handleTxs(Transaction[] possibleTxs) {
         List<Transaction> legitTransaction = new ArrayList<Transaction>();
-		for (int i = 0; i < possibleTxs.size(); i++) {
-            Transaction tx = possibleTxs[i];
-            if (isValidTx(tx)) {
-                legitTransaction.add(tx);
-            }
-            //Txn is legit, now do book keeping and update UTXO pool
-            List<Transaction.Input> inputs = tx.getInputs();
-		    for (int j = 0; j < inputs.size(); j++) {
+        for (int i = 0; i < possibleTxs.length; i++) {
+          Transaction tx = possibleTxs[i];
+          if (isValidTx(tx)) {
+              legitTransaction.add(tx);
+          }
+          //Txn is legit, now do book keeping and update UTXO pool
+          List<Transaction.Input> inputs = tx.getInputs();
+  		    for (int j = 0; j < inputs.size(); j++) {
                 Transaction.Input input = inputs.get(j);
                 UTXO utxo = new UTXO(input.prevTxHash, input.outputIndex);
                 utxoPool.removeUTXO(utxo);//remove used coins from available pool
-            }
-            List<Transaction.Output> outputs = tx.getOutputs();
-		    for (int j = 0; j < outputs.size(); j++) {
-                Transaction.Output output = outputs.get(j);
-                UTXO utxo = new UTXO(tx.getHash(), j);
-                utxoPool.addUTXO(utxo, output);//add created coins to available pool
-            }
-            Transaction[] resp = new Transaction[legitTransaction.size()];
-		    legitTransaction.toArray(resp);
-		    return resp; // Return array of valid tx
-        }
+              }
+          List<Transaction.Output> outputs = tx.getOutputs();
+  		    for (int j = 0; j < outputs.size(); j++) {
+                  Transaction.Output output = outputs.get(j);
+                  UTXO utxo = new UTXO(tx.getHash(), j);
+                  utxoPool.addUTXO(utxo, output);//add created coins to available pool
+                }
+          }
+          Transaction[] resp = new Transaction[legitTransaction.size()];
+          legitTransaction.toArray(resp);
+          return resp; // Return array of valid tx
     }
+
+    /****----- Private methods -----******/
     private boolean verifySignatureOfCoinBeingUsed(Transaction tx, int index, Transaction.Input input) {
 		UTXO utxo = new UTXO(input.prevTxHash, input.outputIndex);
 		Transaction.Output correspondingOutput = utxoPool.getTxOutput(utxo);
